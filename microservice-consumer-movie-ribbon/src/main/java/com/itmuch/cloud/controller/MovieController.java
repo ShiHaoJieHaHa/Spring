b7f12d4e06c9947bf90c5290408cd06e416695fd
shihaojieHaHa <2690631906@qq.com>
2021-06-28 16:37:29 +0800

    microservice-consumer-movie-ribbon服务增加LoadBalancerClient

--- microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -3,6 +3,8 @@ package com.itmuch.cloud.controller;
 import com.itmuch.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,10 @@ public class MovieController {
   @Autowired
   private RestTemplate restTemplate;
 
-  //@Value("${user.userServicePath}")
-  //private String userServicePath;
+  @Value("${user.userServicePath}")
+  private String userServicePath;
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
 
 
   // http://localhost:7900/simple/
@@ -24,5 +28,11 @@ public class MovieController {
   public User findById(@PathVariable Long id) {
     //return this.restTemplate.getForObject(this.userServicePath + id, User.class);
     return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+  }
+
+  @GetMapping("/test")
+  public  String  test() {
+    ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+    return "";
   }
 }
