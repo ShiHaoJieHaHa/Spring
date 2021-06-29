4f32e38d9f51fe02a0da3674d1af3647e4bfb2bc
shihaojieHaHa <2690631906@qq.com>
2021-06-29 11:25:29 +0800

    microservice-consumer-movie-ribbon服务TestConfiguration修改

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/TestConfiguration.java
@@ -0,0 +1,22 @@
+package com.itmuch.cloud;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ExcludeFromComponentScan
+public class TestConfiguration {
+
+   /* @Autowired
+    IClientConfig  config;*/
+
+
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
+}
