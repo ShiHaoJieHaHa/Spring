fe11958e420fbe237e7f03fc41beb39a1f3e4f9a
shihaojieHaHa <2690631906@qq.com>
2021-07-29 08:40:40 +0800

    controller的提交

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.itmuch.cloud.entity.User;
 import com.itmuch.cloud.feign.UserFeignClient;
 
@@ -19,7 +18,7 @@ public class MovieController {
     return this.userFeignClient.findById(id);
   }
 
-  @GetMapping("/test")
+  @GetMapping("/user")
   public User testPost(User user) {
     return this.userFeignClient.postUser(user);
   }
