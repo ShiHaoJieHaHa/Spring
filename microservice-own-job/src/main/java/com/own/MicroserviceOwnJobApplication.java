160b70fa567e2f0d6c639b1cf74e0d2085381e7c
shihaojie <dmlwxhn1314>
2021-10-27 10:55:01 +0800

    microservice-own-job项目的启动类

---
+++ microservice-own-job/src/main/java/com/own/MicroserviceOwnJobApplication.java
@@ -0,0 +1,16 @@
+package com.own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCaching
+public class MicroserviceOwnJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceOwnJobApplication.class, args);
+  }
+}
