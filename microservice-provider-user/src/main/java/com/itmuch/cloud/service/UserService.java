d856b325bb1fecf93b0fcf149f28291f13644284
shihaojie <dmlwxhn1314>
2021-10-28 10:01:12 +0800

    UserServiceImpl和UserService的编写

---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/service/UserService.java
@@ -0,0 +1,8 @@
+package com.itmuch.cloud.service;
+
+import com.itmuch.cloud.entity.User;
+
+public interface UserService {
+    User findById(Long id);
+
+}
---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.itmuch.cloud.service.impl;
+
+import com.itmuch.cloud.entity.User;
+import com.itmuch.cloud.repository.UserRepository;
+import com.itmuch.cloud.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl  implements UserService {
+    private  static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public User findById(Long id) {
+        logger.info("根据ID调用接口");
+        return userRepository.findOne(id);
+    }
+}
