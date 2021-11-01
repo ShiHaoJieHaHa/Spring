bccd57e2b8d10f8e9748c62e552618df8068b500
shihaojie <dmlwxhn1314>
2021-11-01 09:09:17 +0800

    PersonRepository

---
+++ microservice-own-job/src/main/java/com/own/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.own.repository;
+
+import com.own.pojo.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long> {
+}
