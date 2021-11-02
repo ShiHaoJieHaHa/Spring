fe8425df31a6a8fde47edcb45232afd4aecd4c96
shihaojie <dmlwxhn1314>
2021-11-02 15:08:36 +0800

    UserController

--- microservice-provider-users/src/main/java/com/demo/controller/UserController.java
+++ microservice-provider-users/src/main/java/com/demo/controller/UserController.java
@@ -5,60 +5,82 @@ import com.demo.repository.UserRepository;
 import com.google.common.collect.Lists;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.demo.config.SwaggerConfig;
+
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Api(tags =SwaggerConfig.Use_info)
+@Validated
+@Slf4j
 @RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-  @Autowired
-  private UserRepository userRepository;
 
-  @Autowired
-  private EurekaClient eurekaClient;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-  @Autowired
-  private DiscoveryClient discoveryClient;
+    @Autowired
+    private UserRepository userRepository;
 
-  @GetMapping("/simple/{id}")
-  public User findById(@PathVariable Long id) {
-    return this.userRepository.findOne(id);
-  }
+    @Autowired
+    private EurekaClient eurekaClient;
 
-  @GetMapping("/eureka-instance")
-  public String serviceUrl() {
-    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
-    return instance.getHomePageUrl();
-  }
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-  @GetMapping("/instance-info")
-  public ServiceInstance showInfo() {
-    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
-    return localServiceInstance;
-  }
-  // 该请求不会成功
-  @GetMapping("/get-user")
-  public User getUser(User user) {
-    return user;
-  }
+    @GetMapping("/simple/{id}")
+    public User findById(@PathVariable Long id) {
+        if (userRepository.findById(id).isPresent()) {
+            logger.info("返回数据成功");
+            return this.userRepository.findById(id).get();
+        } else {
+            logger.error("没有查询到id对应的数据");
+        }
+        return null;
+    }
 
-  @GetMapping("list-all")
-  public List<User> listAll() {
-    ArrayList<User> list = Lists.newArrayList();
-    User user = new User(1L, "zhangsan");
-    User user2 = new User(2L, "zhangsan");
-    User user3 = new User(3L, "zhangsan");
-    list.add(user);
-    list.add(user2);
-    list.add(user3);
-    return list;
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
 
-  }
+    @GetMapping("/instance-info")
+    public List<String> showInfo() {
+        //ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        List<String> localServiceInstance = discoveryClient.getServices();
+        logger.info("获取到localServiceInstance的列表数据");
+        return localServiceInstance;
+    }
+
+    // 该请求不会成功
+    @GetMapping("/get-user")
+    public User getUser(User user) {
+        return user;
+    }
+
+    @GetMapping("list-all")
+    public List<User> listAll() {
+        ArrayList<User> list = Lists.newArrayList();
+        User user = new User(1L, "zhangsan");
+        User user2 = new User(2L, "zhangsan");
+        User user3 = new User(3L, "zhangsan");
+        list.add(user);
+        list.add(user2);
+        list.add(user3);
+        return list;
+    }
 }
