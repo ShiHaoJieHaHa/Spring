a5c9336823d051ec22e13bd3d530110015c0726d
shihaojie <dmlwxhn1314>
2021-11-03 17:35:48 +0800

    UserController

--- microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -7,24 +7,27 @@ import java.util.Map;
 
 import com.itmuch.cloud.service.UserService;
 import com.itmuch.cloud.utils.AtomicCounter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.google.common.collect.Lists;
 import com.itmuch.cloud.entity.User;
 import com.itmuch.cloud.repository.UserRepository;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import javax.validation.constraints.NotNull;
 
+@Slf4j
+@Validated
 @RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
     @Autowired
@@ -56,7 +59,7 @@ public class UserController {
     }
 
     @GetMapping("/service")
-    public User findId(Long id) {
+    public User findId(@RequestParam @NotNull Long id) {
         return service.findById(id);
     }
 
