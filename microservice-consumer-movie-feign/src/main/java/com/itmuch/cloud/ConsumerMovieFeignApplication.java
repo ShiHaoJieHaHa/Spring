fe44db56f7b461b5c86edce20992488dabd1d3cb
shihaojie <dmlwxhn1314>
2021-11-03 09:27:16 +0800

    ConsumerMovieFeignApplication启动类

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/ConsumerMovieFeignApplication.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/ConsumerMovieFeignApplication.java
@@ -2,12 +2,14 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ConsumerMovieFeignApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConsumerMovieFeignApplication.class, args);
