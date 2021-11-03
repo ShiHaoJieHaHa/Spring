46b895a3479395ac152db2424ee326cfe0513732
shihaojie <dmlwxhn1314>
2021-11-03 09:27:35 +0800

    MovieController

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -1,5 +1,7 @@
 package com.itmuch.cloud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itmuch.cloud.entity.User;
 import com.itmuch.cloud.feign.UserFeignClient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class MovieController {
-
+    private static  final Logger logger = LoggerFactory.getLogger(MovieController.class);
   @Autowired
   private UserFeignClient userFeignClient;
 
   @GetMapping("/movie/{id}")
-  public User findById(@PathVariable Long id) {
-    return this.userFeignClient.findById(id);
+  public Map<String, Object> findById(@PathVariable Long id) {
+    Map<String,Object> map=new HashMap<>();
+    map.put("user",this.userFeignClient.findById(id));
+    logger.info("feign调用到服务提供的数据");
+    return map;
   }
 
   @GetMapping("/user")
