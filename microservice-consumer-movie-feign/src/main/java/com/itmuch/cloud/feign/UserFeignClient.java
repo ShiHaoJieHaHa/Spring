005dc501c2b881a1b1e814c90c936ff05ddc702c
shihaojie <dmlwxhn1314>
2021-11-03 09:28:00 +0800

    UserFeignClient

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/feign/UserFeignClient.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/feign/UserFeignClient.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud.feign;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.itmuch.cloud.entity.User;
 
+import java.util.Map;
+
 @FeignClient("microservice-provider-user")
 public interface UserFeignClient {
   @RequestMapping(value = "/simple/{id}", method = RequestMethod.GET)
-  public User findById(@PathVariable("id") Long id); // 两个坑：1. @GetMapping不支持   2. @PathVariable得设置value
+  public Map<String,Object> findById(@PathVariable("id") Long id); // 两个坑：1. @GetMapping不支持   2. @PathVariable得设置value
   
   @RequestMapping(value = "/user", method = RequestMethod.POST)
   public User postUser(@RequestBody User user);
