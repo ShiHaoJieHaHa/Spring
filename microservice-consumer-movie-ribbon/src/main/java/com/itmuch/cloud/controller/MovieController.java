3c3c43f707a0ac74cf50945fc2dc9ab75e03b1bc
shihaojie <dmlwxhn1314>
2021-11-03 10:31:59 +0800

    MovieController

--- microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -1,20 +1,26 @@
 package com.itmuch.cloud.controller;
 
-import com.itmuch.cloud.entity.User;
+
+import com.itmuch.cloud.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import java.util.Map;
 
 @RestController
 public class MovieController {
+    private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private MovieService movieService;
     // @Value("${user.userServicePath}")
     // private String userServicePath;
     @Autowired
@@ -25,9 +31,14 @@ public class MovieController {
     // VIP virtual IP   虚拟ip
     // HAProxy Heartbeat
     @GetMapping("/movie/{id}")
-    public User findById(@PathVariable Long id) {
+    public Map<String, Object> findById(@PathVariable Long id) {
         //return this.restTemplate.getForObject(this.userServicePath + id, User.class);
-        return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+       /* Map<String, Object> map = new HashMap<>();
+        map.put("user", this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, Map.class));
+        */
+        logger.info("ribbon服务已获取到数据");
+        return movieService.getUserInfo(id);
+        //       return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
     }
 
     @GetMapping("/test")
