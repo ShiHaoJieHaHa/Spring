0eb9239e25bc9f1e4b5ca9c73b677ffecaf6e261
shihaojie <dmlwxhn1314>
2021-11-03 10:32:59 +0800

    microserviceConsumerMovieRibbonApplication启动类

--- microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/microserviceConsumerMovieRibbonApplication.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/microserviceConsumerMovieRibbonApplication.java
@@ -2,8 +2,10 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @RibbonClient(name = "microservice-provider-user",configuration = TestConfiguration.class)
 @ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class) })
+@EnableDiscoveryClient
+@EnableHystrix
 public class microserviceConsumerMovieRibbonApplication {
 
   @Bean
