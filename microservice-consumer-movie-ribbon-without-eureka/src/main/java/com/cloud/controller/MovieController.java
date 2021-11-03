f52e85a14db5355a5d251383bc4a817ba35c93bf
shihaojie <dmlwxhn1314>
2021-11-03 15:07:47 +0800

    MovieController

--- microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/cloud/controller/MovieController.java
+++ microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/cloud/controller/MovieController.java
@@ -1,40 +1,54 @@
 package com.cloud.controller;
 
+import com.cloud.service.MovieService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import java.util.Map;
 
-import com.cloud.entity.User;
 
 @RestController
+@Slf4j
+@Validated
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MovieController {
-  @Autowired
-  private RestTemplate restTemplate;
-  @Autowired
-  private LoadBalancerClient loadBalancerClient;
 
-  @GetMapping("/movie/{id}")
-  public User findById(@PathVariable Long id) {
-    // http://localhost:7900/simple/
-    // VIP virtual IP
-    // HAProxy Heartbeat
+    private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+    /* @Autowired
+     private RestTemplate restTemplate;*/
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private MovieService movieService;
 
-    ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
-    System.out.println("===" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+    @GetMapping("/movie/{id}")
+    public Map<String, Object> findById(@PathVariable Long id) {
+        // http://localhost:7900/simple/
+        // VIP virtual IP
+        // HAProxy Heartbeat
 
-    return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
-  }
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        System.out.println("===" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+        logger.info("microservice-consumer-movie-ribbon-withput-eureka获取数据");
+        return movieService.getUserInfo(id);
+        // return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+    }
 
-  @GetMapping("/test")
-  public String test() {
-    ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
-    System.out.println("111" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
-   ServiceInstance serviceInstance2 = this.loadBalancerClient.choose("microservice-provider-users");
-    System.out.println("222" + ":" + serviceInstance2.getServiceId() + ":" + serviceInstance2.getHost() + ":" + serviceInstance2.getPort());
-    return "1";
-  }
+    @GetMapping("/test")
+    public String test() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        System.out.println("111" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+        ServiceInstance serviceInstance2 = this.loadBalancerClient.choose("microservice-provider-users");
+        System.out.println("222" + ":" + serviceInstance2.getServiceId() + ":" + serviceInstance2.getHost() + ":" + serviceInstance2.getPort());
+        return "1";
+    }
 }
