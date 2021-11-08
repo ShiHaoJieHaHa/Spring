584ed80aff64358c9140aae32444997730052128
shihaojie <dmlwxhn1314>
2021-11-08 15:52:23 +0800

    MovieController

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -1,5 +1,8 @@
 package com.itmuch.cloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +17,38 @@ import java.util.Map;
 
 @RestController
 public class MovieController {
-    private static  final Logger logger = LoggerFactory.getLogger(MovieController.class);
-  @Autowired
-  private UserFeignClient userFeignClient;
+    private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+    @Autowired
+    private UserFeignClient userFeignClient;
 
-  @GetMapping("/movie/{id}")
-  public Map<String, Object> findById(@PathVariable Long id) {
-    Map<String,Object> map=new HashMap<>();
-    map.put("user",this.userFeignClient.findById(id));
-    logger.info("feign调用到服务提供的数据");
-    return map;
-  }
 
-  @GetMapping("/user")
-  public User testPost(User user) {
-    return this.userFeignClient.postUser(user);
-  }
+    @GetMapping("/movie/{id}")
+    @HystrixCommand(fallbackMethod = "errorById",commandProperties =
+            { @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds"
+             ,value = "3000")
+    })
+    public Map<String, Object> findById(@PathVariable Long id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", this.userFeignClient.findById(id));
+        logger.info("feign调用到服务提供的数据");
+        return map;
+    }
 
-  @GetMapping("/test-get")
-  public User testGet(User user) {
-    return this.userFeignClient.getUser(user);
-  }
+    private Map<String, Object> errorById(@PathVariable Long id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("logger", "数据不存在");
+        logger.info("调用errorById方法");
+        return map;
+    }
+
+
+    @GetMapping("/user")
+    public User testPost(User user) {
+        return this.userFeignClient.postUser(user);
+    }
+
+    @GetMapping("/test-get")
+    public User testGet(User user) {
+        return this.userFeignClient.getUser(user);
+    }
 }
