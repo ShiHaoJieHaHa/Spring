3ed32823d4c207927648e75c7837634b4d7a7947
shihaojie <dmlwxhn1314>
2021-11-11 17:37:47 +0800

    AtomicCounter

---
+++ own-Factory/src/main/java/com/demo/factory/utils/AtomicCounter.java
@@ -0,0 +1,28 @@
+package com.demo.factory.utils;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicCounter {
+    private static final AtomicCounter atomicCounter = new AtomicCounter();
+    private AtomicCounter() {
+
+    }
+    public static AtomicCounter getInstance() {
+        return atomicCounter;
+    }
+
+    private static AtomicInteger counter = new AtomicInteger();
+
+    public int getValue() {
+        return counter.get();
+    }
+
+    public int increase() {
+        return counter.incrementAndGet();
+    }
+
+    public int decrease() {
+        return counter.decrementAndGet();
+    }
+
+}
