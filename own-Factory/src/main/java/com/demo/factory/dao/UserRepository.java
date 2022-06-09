ad2df2df8be8a471f37acf8afc24748abd4bd045
shihaojie <dmlwxhn1314>
2022-06-09 14:04:29 +0800

    UserRepository增加getUserName

--- own-Factory/src/main/java/com/demo/factory/dao/UserRepository.java
+++ own-Factory/src/main/java/com/demo/factory/dao/UserRepository.java
@@ -2,8 +2,15 @@ package com.demo.factory.dao;
 
 import com.demo.factory.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query(value = "SELECT *  from user where name=:name",nativeQuery = true)
+    User getUserName(@Param(value = "name") String name);
+
+
 }
